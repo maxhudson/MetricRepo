486342687c154f1a14d5c7e263c160de9914b0cc
chanceofthat <chancesofthat@gmail.com>
2015-02-04 22:39:20 -0800

    Finished Add Metric Page with finalized buttons, colors and images.

--- Metric/MetricManagerViewController.swift
+++ Metric/MetricManagerViewController.swift
@@ -13,23 +13,34 @@ class MetricManagerViewController: UIViewController {
    override func viewDidLoad() {
       super.viewDidLoad()
       metricTextField.becomeFirstResponder()
-
+      setupView()
    }
 
    var newMetric: Metric!
    
+   @IBOutlet weak var trashButton: UIButton!
+   @IBOutlet weak var doneButton: UIButton!
+   @IBOutlet var metricManagerView: UIView!
    @IBOutlet weak var metricTextField: UITextField!
    
    @IBAction func trashMetric(sender: AnyObject) {
       dismissViewControllerAnimated(true, completion: nil)
    }
+   
+   func setupView(){
+      doneButton.backgroundColor = Helper.darkNayColor
+      metricManagerView.backgroundColor = Helper.goldColor
+      
+   }
 
    override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
-      self.view.endEditing(true)
+//      self.view.endEditing(true)
    }
-   
+
    
    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+      self.view.endEditing(true)
+
       if segue.identifier == "DoneMetric" {
          if let title = metricTextField.text {
             if !title.isEmpty{
